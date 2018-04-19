d7fefdc6e7319c6ec4ee7e642b5a719aec196961
tullinge1996 <31662409+tullinge1996@users.noreply.github.com>
2018-04-19 18:02:48 +0200

    added pictures to footer

--- release/src/Footer.js
+++ release/src/Footer.js
@@ -1,14 +1,25 @@
 import React, {Component} from "react";
 import "./App.css";
 import { Container, Row, Col } from 'reactstrap';
+import Images from "./IMAGES/Images"
+var {fbLogga, googlePlusLogga, linkedinLogga, twitterLogga} = Images
 
 class Footer extends Component {
   render(){
     return(
       <Container className="FooterContainer">
       <Row className = "RowFooter">
-          <Col xl= "12" className = "ColFooter">
-          <h3>Footer</h3>
+          <Col xl= "6" className = "ColFooter">
+          <ul>
+          <li><a href =""><img src={twitterLogga}alt="Twitterlogo"/></a></li>
+          <li><a href =""><img src={googlePlusLogga}alt="GooglePlusLogo"/></a></li>
+          <li><a href =""><img src={linkedinLogga}alt="LinkedInLogo"/></a></li>
+          <li><a href =""><img src={fbLogga} alt="FaceBookLogo"/></a></li>
+          </ul>
+          </Col>
+          <Col xl= "6" className = "ColFooter2">
+          <h4>Get In Touch</h4>
+          <p>If you have any questions, please feel free to Email me at <strong>carl.brunngard@student.kyh.se</strong> and I<br></br> will get back to you as soon as I can.</p>
           </Col>
         </Row>
       </Container>
---
+++ release/src/IMAGES/Images.js
@@ -0,0 +1,6 @@
+export default{
+  fbLogga: require("./fblogga.png"),
+  googlePlusLogga: require("./googlepluslogga.png"),
+  linkedinLogga: require("./linkedinlogga.png"),
+  twitterLogga: require("./twitterlogga.png")
+}
